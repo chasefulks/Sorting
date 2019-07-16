ec007076f53d78b85a0b09e0f19eeb3208bd7b28
chasefulks <chase.fulks@gmail.com>
2019-07-16 18:57:16 -0500

    iterative and recursive solution

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,14 +3,29 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    while max(len(arrA), len(arrB)) > 0:
+        for i in range(len(merged_arr)):
+            if len(arrA) == 0:
+                merged_arr[i] = arrB.pop(0)
+            elif len(arrB) == 0:
+                merged_arr[i] = arrA.pop(0)
+            elif arrA[0] < arrB[0]:
+                merged_arr[i] = arrA.pop(0)
+            else:  # arrA[0] > arrB[0]
+                merged_arr[i] = arrB.pop(0)
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        # halve the array
+        arrA = merge_sort(arr[:len(arr)//2])
+        arrB = merge_sort(arr[len(arr)//2:])
 
+        arr = merge(arrA, arrB)
     return arr
 
 
